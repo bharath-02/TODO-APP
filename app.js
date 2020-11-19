eb358e3c2166ea9cd00083da47509edd83e7b132
bhara <bharathJ222000@gmail.com>
2020-11-19 13:22:34 +0530

    added

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// Import required packages
 var express = require('express');
 
+// Create App
 var app = express();
+
+// Set up template engines
+app.set('view engine', 'ejs');
+
+// Create static files
+app.use(express.static('./public'));
+
+// Listen to port
+app.listen(3300, () => {
+    console.log('Listening to port 3300');
+})
