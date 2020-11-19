39f6194bf17e282ce73c99cffc518f85a88a32bb
bhara <bharathJ222000@gmail.com>
2020-11-19 13:31:41 +0530

    added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import required packages
 var express = require('express');
+var todoController = require('./controllers/todoController');
 
 // Create App
 var app = express();
@@ -10,6 +11,8 @@ app.set('view engine', 'ejs');
 // Create static files
 app.use(express.static('./public'));
 
+// Fire the controllers
+todoController(app);
 // Listen to port
 app.listen(3300, () => {
     console.log('Listening to port 3300');
