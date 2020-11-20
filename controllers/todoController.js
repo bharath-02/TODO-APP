0821b46d7ef48aa2ec7e8cb1e06f8da897ef7f2f
bhara <bharathJ222000@gmail.com>
2020-11-20 08:38:20 +0530

    added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,7 @@
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'kick some coding ass' }];
 
 module.exports = (app) => {
@@ -5,11 +9,12 @@ module.exports = (app) => {
         res.render('todo', { todos: data });
     })
 
-    app.post('/todo', (req, res) => {
-
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        res.json(data);
     })
 
-    app.delete('/todo', (req, res) => {
+    app.delete('/todo/:item', (req, res) => {
 
     })
 };
